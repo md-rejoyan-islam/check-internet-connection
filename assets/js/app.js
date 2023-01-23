ca60b914fdb24e6c30aa42f3e9bc9e994a0ee216
RejoyanIslam <rejoyanislam0014@gmail.com>
2023-01-23 21:23:28 +0600

    upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,53 +1,39 @@
-const online=document.getElementById('online')
-const offline=document.getElementById('offline')
-const timer=document.getElementById('timer')
-const reconnect=document.getElementById('reconnect')
+const online = document.getElementById("online");
+const offline = document.getElementById("offline");
+const timer = document.getElementById("timer");
+const reconnect = document.getElementById("reconnect");
 
-let time=10
-let clear=''
-const again=onoffline=(e)=>{
-    offline.classList.remove("hidden");
-    time=10
-     clear = setInterval(() => {
-      time--;
+let time = 10;
+let clear = "";
+const again = (onoffline = (e) => {
+  offline.classList.remove("hidden");
+  time = 10;
+  clear = setInterval(() => {
+    time--;
 
+    timer.innerHTML = time;
+    if (time == 0) {
+      time = 10;
       timer.innerHTML = time;
-      if (time == 0) {
-        time = 10;
-        timer.innerHTML = time;
-        offline.classList.add("hidden");
+      offline.classList.add("hidden");
 
-        clearInterval(clear);
-      }
-    }, 1000);
- 
-}
+      clearInterval(clear);
+    }
+  }, 1000);
+});
 
+reconnect.onclick = () => {
+  time = 10;
+  timer.innerHTML = time;
+  clearInterval(clear);
+  again();
+};
 
-
-
-
-
-
-
-reconnect.onclick=()=>{
-  time=10
-timer.innerHTML = time;
-    clearInterval(clear)
-again()
-
-}
-
-
-ononline=(e)=>{
-  offline.classList.add('hidden');
-  time=1
-    online.classList.remove('hidden')
-    setTimeout(() => {
-   
-      online.classList.add("hidden");
-    }, 2000);
-}
-
-
-
+ononline = (e) => {
+  offline.classList.add("hidden");
+  time = 1;
+  online.classList.remove("hidden");
+  setTimeout(() => {
+    online.classList.add("hidden");
+  }, 2000);
+};
